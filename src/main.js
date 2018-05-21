e206a44fab06bb480ee6598b102324dd74f0de4c
Herbie <843834089@qq.com>
2018-05-21 20:37:00 +0800

    chalk命令行打印颜色插件

--- src/main.js
+++ src/main.js
@@ -1,2 +1,31 @@
 
-console.log(6)
+const log = console.log;
+console.log(log)
+
+/**
+ * chalk 颜色的插件
+ */
+const chalk = require('chalk');
+console.log(chalk.blue('Hello world!'));
+console.log(chalk.green('Hello world!'));
+log(chalk.red('Hello world!'));
+log(chalk.yellow('Hello world!'));
+log(chalk.red('Hello', chalk.underline.bgBlue('world') + '!'));
+log(chalk.green(
+    'I am a green line ' +
+    chalk.blue.underline.bold('with a blue substring') +
+    ' that becomes green again!'
+));
+
+// ES2015 template literal 
+log(`
+CPU: ${chalk.red('90%')}
+RAM: ${chalk.green('40%')}
+DISK: ${chalk.yellow('70%')}
+`);
+
+// Use RGB colors in terminal emulators that support it. 
+log(chalk.keyword('orange')('Yay for orange colored text!'));
+log(chalk.rgb(123, 45, 67).underline('Underlined reddish color'));
+log(chalk.hex('#DEADED').bold('Bold gray!'));
+// console.log(6)
--- webpack.development.config.js
+++ webpack.development.config.js
@@ -2,11 +2,11 @@ const path = require('path');
 
 const config = {
     mode: 'development',
-    entry: './src/main.js',
-    output: {
-      path: path.resolve(__dirname, 'dist2'),
-      filename: 'my-first-webpack.bundle.js'
-    }
+    // entry: './src/main.js',  //默认 ./src/index.js
+    // output: {
+    //   path: path.resolve(__dirname, 'dist2'),   //默认 dist/main.js
+    //   filename: 'my-first-webpack.bundle.js'
+    // }
 };
 
 module.exports = config;
